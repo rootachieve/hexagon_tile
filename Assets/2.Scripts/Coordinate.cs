00dd0f9b54c7e1d345b236e752c7774325ec7c27
K_Gs:D <rootachieve6053@gmail.com>
2023-03-06 00:17:25 +0900

    feat : 좌표 클래스 생성

---
+++ Assets/2.Scripts/Coordinate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coordinate : MonoBehaviour
+{
+    public int X;
+    public int Y;
+
+    public Coordinate(int x,int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
